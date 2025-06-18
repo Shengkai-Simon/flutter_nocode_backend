63cf0ae69de662f144152d5307e24d61a24374f9
Simon Yang <simonskyang@gmail.com>
2025-06-17 23:23:42 -0600

    Add `project-service` to microservices architecture

--- db/initdb/create-dbs.sql
+++ db/initdb/create-dbs.sql
@@ -3,4 +3,5 @@
 -- Create individual databases for each microservice
 CREATE DATABASE authdb;
 CREATE DATABASE userdb;
+CREATE DATABASE projectdb;
 CREATE DATABASE notificationdb;
---
+++ microservices/project-service/src/main/java/dev/skyang/projectservice/config/ApiPaths.java
@@ -0,0 +1,10 @@
+package dev.skyang.projectservice.config;
+
+public final class ApiPaths {
+
+    private ApiPaths() { }
+
+    // --- Base Paths ---
+    public static final String API_BASE = "/api";
+    public static final String PUBLIC_SUB_PATH = "/public";
+}
