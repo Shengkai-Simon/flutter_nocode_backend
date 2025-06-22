235988fbb0bdff6b341ad3b76f5763be0c39b92e
Simon Yang <simonskyang@gmail.com>
2025-06-22 14:23:17 -0600

    use RSA key pair for JWT signing to prevent token invalidation on restart

--- microservices/auth-service/src/main/java/dev/skyang/authservice/AuthServiceApplication.java
+++ microservices/auth-service/src/main/java/dev/skyang/authservice/AuthServiceApplication.java
@@ -2,10 +2,12 @@ package dev.skyang.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ConfigurationPropertiesScan
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
