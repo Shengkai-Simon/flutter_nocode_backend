f07aff15bc5873cc07a1d94db201ad66b3d96783
Simon Yang <simonskyang@gmail.com>
2025-06-23 14:53:27 -0600

    move token storage from frontend to httpOnly cookie on backend

--- microservices/auth-service/src/main/java/dev/skyang/authservice/dto/LoginResponse.java
+++
@@ -1,9 +0,0 @@
-package dev.skyang.authservice.dto;
-
-public class LoginResponse {
-    private String accessToken;
-    public LoginResponse(String accessToken) { this.accessToken = accessToken; }
-    // Getter and Setter
-    public String getAccessToken() { return accessToken; }
-    public void setAccessToken(String accessToken) { this.accessToken = accessToken; }
-}
